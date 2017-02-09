5a03b92a4c8ac2535fe43b6f7187e622d36110a7
yinghuiLiu <908020541@qq.com>
2017-02-09 16:00:33 +0800

    添加工程

---
+++ langnuo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  langnuo
+//
+//  Created by liuyinghui on 16/2/21.
+//  Copyright © 2016年 liuyinghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+
+@end
+
---
+++ langnuo/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  langnuo
+//
+//  Created by liuyinghui on 16/2/21.
+//  Copyright © 2016年 liuyinghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+
+
+
+
+@end
+
