83d2b4fd254fa25f34c3b14e3ed8123aa5f4d61c
liuyinghui <liuyinghui@donghangjinrong.local>
2017-03-14 09:48:59 +0800

    框架修改

---
+++ langnuo/Control/langnuoTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  langnuoTabBarViewController.h
+//  langnuo
+//
+//  Created by liuyinghui on 2017/3/14.
+//  Copyright © 2017年 liuyinghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface langnuoTabBarViewController : UITabBarController
+
+@end
